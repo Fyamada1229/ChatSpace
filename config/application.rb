23e942ef9382fd3397ffd00f3b859c8007d668c0
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-05-05 21:05:01 +0900

    グループ新規機能　追記

--- config/application.rb
+++ config/application.rb
@@ -8,29 +8,18 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-<<<<<<< HEAD
-<<<<<<< Updated upstream
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-=======
-=======
->>>>>>> eb46d0d75bc11fb43fb26e46bb5383d3c50cdd69
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
-<<<<<<< HEAD
       config.i18n.default_locale = :ja
     end
->>>>>>> Stashed changes
-=======
-    end
->>>>>>> eb46d0d75bc11fb43fb26e46bb5383d3c50cdd69
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
   root "messages#index"
-
   resources :users, only: [:edit, :update]
->>>>>>> Stashed changes
-=======
-  root "messages#index"
->>>>>>> eb46d0d75bc11fb43fb26e46bb5383d3c50cdd69
+  resources :groups, only: [:new, :create]
 end
